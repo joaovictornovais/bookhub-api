835f049642d5d318d21c867e1646d310ec4517e3
João Victor Novais <joao.dipaay@gmail.com>
2024-04-02 14:17:44 -0300

    style: new email template

--- src/main/java/br/com/joao/library/util/emails/EmailBorrow.java
+++ src/main/java/br/com/joao/library/util/emails/EmailBorrow.java
@@ -3,85 +3,94 @@ package br.com.joao.library.util.emails;
 public class EmailBorrow {
 
     public static String borrow() {
-        return "<html lang=\"pt-br\">\n" +
+        return "<!DOCTYPE html>\n" +
+                "<html lang=\"en\">\n" +
                 "  <head>\n" +
                 "    <meta charset=\"UTF-8\" />\n" +
                 "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n" +
-                "    <title>Lembrete de empréstimo de livro</title>\n" +
+                "    <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\" />\n" +
+                "    <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin />\n" +
+                "    <link\n" +
+                "      href=\"https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap\"\n" +
+                "      rel=\"stylesheet\"\n" +
+                "    />\n" +
                 "    <style>\n" +
-                "      body {\n" +
-                "        font-family: Arial, sans-serif;\n" +
+                "      * {\n" +
                 "        margin: 0;\n" +
                 "        padding: 0;\n" +
-                "        background-color: #f4f4f4;\n" +
+                "        box-sizing: border-box;\n" +
+                "        font-family: \"Roboto\";\n" +
                 "      }\n" +
                 "      .container {\n" +
                 "        max-width: 600px;\n" +
-                "        margin: 0 auto;\n" +
-                "        padding: 20px;\n" +
-                "        background-color: #fff;\n" +
-                "        border-radius: 10px;\n" +
-                "        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);\n" +
                 "      }\n" +
-                "      .header {\n" +
+                "      .header-img {\n" +
+                "        width: 100%;\n" +
+                "        height: 150px;\n" +
+                "        object-fit: cover;\n" +
+                "        border-radius: 5px 5px 0 0;\n" +
+                "      }\n" +
+                "      .body {\n" +
+                "        padding: 20px;\n" +
+                "        background-color: #efefef;\n" +
+                "        display: flex;\n" +
+                "        flex-direction: column;\n" +
+                "        gap: 30px;\n" +
+                "      }\n" +
+                "      h2 {\n" +
+                "        color: rgb(18, 46, 122);\n" +
                 "        text-align: center;\n" +
-                "        margin-bottom: 20px;\n" +
-                "        background: url(\"https://i.pinimg.com/originals/02/25/a4/0225a431e5b2637204873584848f3949.png\")\n" +
-                "          no-repeat center center;\n" +
-                "        background-size: cover;\n" +
-                "        border-top-left-radius: 10px;\n" +
-                "        border-top-right-radius: 10px;\n" +
-                "        padding: 50px 20px;\n" +
-                "        color: #0061c9;\n" +
-                "        text-shadow: 1px 1px 2px black, 0 0 1em black, 0 0 0.2em black;\n" +
-                "        font-size: 1.2rem;\n" +
+                "        font-weight: 600;\n" +
                 "      }\n" +
                 "      .content {\n" +
                 "        text-align: center;\n" +
+                "        align-items: center;\n" +
                 "      }\n" +
-                "      .book-info {\n" +
-                "        margin-bottom: 20px;\n" +
+                "      .content p {\n" +
+                "        margin: 15px 0 15px 0;\n" +
                 "      }\n" +
-                "      .footer {\n" +
-                "        text-align: center;\n" +
-                "        margin-top: 20px;\n" +
-                "        color: #d3d3d3;\n" +
-                "        background-color: #007bff;\n" +
-                "        padding: 20px;\n" +
-                "        border-bottom-left-radius: 10px;\n" +
-                "        border-bottom-right-radius: 10px;\n" +
-                "      }\n" +
-                "      .book {\n" +
+                "      .capa {\n" +
                 "        max-height: 200px;\n" +
                 "        max-width: 150px;\n" +
+                "      }\n" +
+                "      footer {\n" +
+                "        text-align: center;\n" +
+                "        background-color: rgb(18, 46, 122);\n" +
+                "        font-size: 14px;\n" +
+                "        color: #dfdfdf;\n" +
+                "        padding: 10px;\n" +
+                "        border-radius: 0 0 5px 5px;\n" +
+                "      }\n" +
+                "      .please {\n" +
+                "        font-size: 14px;\n" +
                 "      }\n" +
                 "    </style>\n" +
                 "  </head>\n" +
                 "  <body>\n" +
                 "    <div class=\"container\">\n" +
-                "      <div class=\"header\">\n" +
+                "      <img\n" +
+                "        class=\"header-img\"\n" +
+                "        src=\"https://raw.githubusercontent.com/cat-milk/Anime-Girls-Holding-Programming-Books/master/Java/Kuriyama_Mirai_reading_OOP_and_Java.png\"\n" +
+                "      />\n" +
+                "      <main class=\"body\">\n" +
                 "        <h2>Você pegou um livro emprestado na BookHub!</h2>\n" +
-                "      </div>\n" +
-                "      <div class=\"content\">\n" +
-                "        <p>Prezado(a) [[NOME]],</p>\n" +
-                "        <p>\n" +
-                "          Você pegou emprestado o livro <strong>[[TITULO]]</strong> na Livraria\n" +
-                "          BookHub no dia\n" +
-                "          <strong>[[DATAEMPRESTIMO]]</strong> e deve devolver até o dia \n" +
-                "          <strong>[[DATADEVOLVER]]</strong>.\n" +
-                "        </p>\n" +
-                "        <div class=\"book-info\">\n" +
-                "          <img\n" +
-                "            class=\"book\"\n" +
-                "            src=\"[[CAPA]]\"\n" +
-                "            alt=\"[[TITULO]]\"\n" +
-                "          />\n" +
+                "        <div class=\"content\">\n" +
+                "          <p>Olá, [[NOME]]</p>\n" +
+                "          <p>\n" +
+                "            Você pegou emprestado o livro <strong>[[TITULO]]</strong> na\n" +
+                "            Livraria BookHub no dia <strong>[[DATAEMPRESTIMO]]</strong> e deve\n" +
+                "            devolver até o dia <strong>[[DATADEVOLVER]]</strong>.\n" +
+                "          </p>\n" +
+                "          <img class=\"capa\" src=\"[[CAPA]]\" alt=\"[[TITULO]]\" />\n" +
+                "          <p class=\"please\">\n" +
+                "            Por favor, lembre-se de devolver o livro até a data especificada.\n" +
+                "          </p>\n" +
                 "        </div>\n" +
-                "        <p>Por favor, lembre-se de devolver o livro até a data especificada.</p>\n" +
-                "      </div>\n" +
-                "      <div class=\"footer\">\n" +
-                "        <p>Atenciosamente,<br />Livraria BookHub</p>\n" +
-                "      </div>\n" +
+                "      </main>\n" +
+                "      <footer>\n" +
+                "        <p>Atenciosamente,</p>\n" +
+                "        <p class=\"bookhub\">Livraria BookHub</p>\n" +
+                "      </footer>\n" +
                 "    </div>\n" +
                 "  </body>\n" +
                 "</html>\n";
