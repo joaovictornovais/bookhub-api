2a69065c195d8a8ade425355683c38b945636209
João Victor Novais <joao.dipaay@gmail.com>
2024-04-03 13:40:44 -0300

    feat: User controller + fix dto

---
+++ src/main/java/br/com/joao/library/controllers/UserController.java
@@ -0,0 +1,31 @@
+package br.com.joao.library.controllers;
+
+import br.com.joao.library.domain.user.User;
+import br.com.joao.library.domain.user.UserDTO;
+import br.com.joao.library.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok(userService.create(new User(userDTO)));
+    }
+
+}
